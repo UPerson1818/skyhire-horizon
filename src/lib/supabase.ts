515a0112224134e140dc98f7ccc7aba4ad61957a
UPerson1818 <m2109dummy@gmail.com>
2025-02-21 16:48:24 +0530

    Updated credential of supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseKey = 'your-anon-key';
+const supabaseUrl = 'https://mdxzzexvclftxkejnsky.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1keHp6ZXh2Y2xmdHhrZWpuc2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxMzYxNDMsImV4cCI6MjA1NTcxMjE0M30.R7UIWfzZCyN3uLp03XwnFtDOFPwzgUFlnyUbALJetnE';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
